7f09c57611dadc79ac22dc957493d26ba3673459
Florian Sprung <florian@tsf-428-wpa-1-064.epfl.ch>
2018-04-25 12:02:47 +0200

    sql scripts to create and destroy the tables

---
+++ relations/delete_all_tables.sql
@@ -0,0 +1,23 @@
+drop table acts;
+drop table directs;
+drop table produces;
+drop table writes;
+
+drop table biographicalbooks;
+drop table biography;
+drop table person;
+
+drop table cliplinks;
+
+drop table clip_language;
+drop table clip_genre;
+drop table clip_country;
+drop table clip_rating;
+
+drop table released;
+drop table runs;
+drop table language;
+drop table country;
+drop table genre;
+
+drop table clip;
