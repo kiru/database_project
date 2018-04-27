7c01d37ad8846d02955579612bb8867028a8e04d
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2018-04-27 12:44:59 +0200

    show result

--- web/app.py
+++ web/app.py
@@ -1,9 +1,10 @@
+import os
+
 from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -24,15 +25,55 @@ def search():
     engine = createEngine()
 
     sql = text('select COUNTRYNAME from Country where lower(COUNTRYNAME) like :search')
-
     result = engine.execute(sql, search="%{}%".format(input).lower())
-
     names = []
     for row in result:
         print(row[0])
         names.append(row[0])
 
     return render_template('search.html', countries=names, query=input)
+
+
+@app.route('/predefined/<query_nr>/')
+def query_result(query_nr):
+
+    objects = os.listdir('queries')
+
+    o = objects[int(float(query_nr)) - 1]
+    with open(os.path.join('queries', o)) as f:
+        query = f.read()
+
+    engine = createEngine()
+
+    sql = text(query)
+    result = engine.execute(sql)
+
+    r = []
+    for row in result:
+        print(row[0])
+        r.append(row[0])
+
+    return render_template('predefined.html', queries=(read_queries()), result=r)
+
+
+@app.route('/predefined/')
+def predefiend():
+    return render_template('predefined.html', queries=(read_queries()))
+
+
+def read_queries():
+    objects = os.listdir('queries')
+    queries = []
+    for o in objects:
+        with open(os.path.join('queries', o)) as f:
+            first_line = f.readline()
+        queries.append(first_line.replace("-- Name: ", ""))
+    return queries
+
+
+@app.route('/insert/')
+def insert():
+    return render_template('insert-delete.html')
 
 
 def createEngine():
