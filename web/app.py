7af91a45b9207dbae91d6cfc9a90e38bff79d877
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2018-04-27 12:03:38 +0200

    added country search

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
@@ -7,15 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    oracle_connection_string = 'oracle+cx_oracle://{username}:{password}@{hostname}:{port}/{database}'
-    engine = create_engine(oracle_connection_string.format(
-        username='DB2018_G17',
-        password='DB2018_G17',
-        hostname='diassrv2.epfl.ch',
-        port='1521',
-        database='orcldias',
-    ))
-    engine.connect()
+    engine = createEngine()
 
     sql = text('select COUNTRYNAME from Country')
     result = engine.execute(sql)
@@ -25,9 +17,35 @@ def hello_world():
 
     return render_template('index.html', title='Home')
 
+
 @app.route('/search/')
 def search():
-    return render_template('search.html')
+    input = request.args.get('query')
+    engine = createEngine()
+
+    sql = text('select COUNTRYNAME from Country where lower(COUNTRYNAME) like :search')
+
+    result = engine.execute(sql, search="%{}%".format(input).lower())
+
+    names = []
+    for row in result:
+        print(row[0])
+        names.append(row[0])
+
+    return render_template('search.html', countries=names, query=input)
+
+
+def createEngine():
+    oracle_connection_string = 'oracle+cx_oracle://{username}:{password}@{hostname}:{port}/{database}'
+    engine = create_engine(oracle_connection_string.format(
+        username='DB2018_G17',
+        password='DB2018_G17',
+        hostname='diassrv2.epfl.ch',
+        port='1521',
+        database='orcldias',
+    ))
+    engine.connect()
+    return engine
 
 
 if __name__ == '__main__':
