182c0c6f4a38f6f23d66731ae24392f6dd087be0
Hyunjii Cho <justcho5@gmail.com>
2018-05-01 18:59:10 +0200

    Extract spouse name

--- imported_files/person.py
+++ imported_files/person.py
@@ -7,8 +7,19 @@ Created on Thu Apr 26 15:20:57 2018
 """
 
 import pandas as pd
+import re
 
 from sql_engine import get_engine
+
+
+def capt(x):
+    # Capture word between parantheses
+    m = re.search(r"(?<=\')(.*?)(?=\')", x)
+    if m:
+        return m.group(1)
+    else:
+        return x
+
 
 def person_table():
     #read the data
@@ -30,13 +41,19 @@ def person_table():
     df5['Spouse']=df5['Spouse'].fillna('[]')
     dfspouse=pd.concat([pd.Series(row['FullName'],
                                   row['Spouse'].split('|')) for _, row in df5.iterrows()]).reset_index()
+
+    print("AHAHSDF",dfspouse.head())
     dfspouse['index'] = dfspouse['index'].map(lambda x: x.lstrip('[').rstrip(']'))
-    #print(dfspouse['index'])
+    print("SPOUSE\n",dfspouse['index'])
+
+    # Get just the name
+    dfspouse['index'] = dfspouse['index'].str.replace(r"\'\?\'", r"\'unknown'")
+    dfspouse['index'] = dfspouse['index'].apply(capt)
     
     print('Combine all persons...')
     dfall=df1['FullName'].append(df2['FullName'].append(df3['FullName'].append(df4['FullName'].append(df5['FullName']).append(dfspouse['index']))))
     print('Size of all person data combined: ',dfall.shape)
-    
+    print("dd\n", dfall)
     #select only unique entries
     dfu=dfall.drop_duplicates(keep='first')
     print('Size of unique person data: ',dfu.shape)
