55bfbb155dd841eab2870608ac87e9eb4b5025fd
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2018-05-01 17:44:14 +0200

    added all files which need to be imported

--- imported_files/import.sh
+++ imported_files/import.sh
@@ -10,10 +10,26 @@ python genre.py
 
 python clip.py
 python clip_rating.py
-python clip_language.py
-python clip_country.py
+#python clip_language.py
+#python clip_country.py
+#python clip_genre.py
+#python clip_links.py
 
-python runs.py
+#python runs.py
 
-python directs.py
-python acts.py
+#python person.py
+#python biography.py
+#python directs.py
+#python acts.py
+#python writes.py
+#python produces.py
+
+#python biography.py
+#python released.py
+#python runs.py
+
+#Imported tables into Oracle DB:
+# - Language
+# - Genre
+# - Country
+
