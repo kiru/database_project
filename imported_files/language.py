3ec08e9b5392fd62fb0bee87a3bccfd814cece7b
Hyunjii Cho <justcho5@gmail.com>
2018-05-02 17:28:40 +0200

    Added more cleaning

--- imported_files/language.py
+++ imported_files/language.py
@@ -7,8 +7,19 @@ Created on Thu Apr 26 15:20:57 2018
 """
 
 import pandas as pd
+import re
 
 from sql_engine import get_engine
+
+
+def capt(x):
+    # Capture word between parantheses
+    m = re.search(r"(?<=\()(.*?)(\sversion)?(\sonly)?(?=\))", x)
+    if m:
+        return m.group(1)
+    else:
+        return x
+
 
 def language_table():
     #read the data
@@ -23,18 +34,24 @@ def language_table():
     
     #select only unique entries
     dfu=df.drop_duplicates(subset=['Language'],keep='first')
-    dfu['Language']=dfu['Language'].str.encode('utf-8') #encode strings as unicode for accents etc.
-    print(dfu.shape)
-    
+
+    dfu['Language'] = dfu.Language.str.lower()
+    dfu['Language'] = dfu['Language'].astype('str')
+    dfu['Language'] = dfu['Language'].apply(capt)
+
+    languages = pd.Series(sorted(dfu.Language.unique()))
+    # dfu['Language']=dfu['Language'].str.encode('utf-8') #encode strings as unicode for accents etc.
+
     #reset the index and put it into ClipId
-    dfi=dfu.reset_index(drop=True)
-    dfi['ClipId']=dfi.index
+    id=languages.reset_index(drop=True)
+    # dfi['ClipId']=dfi.index
+    dfi = pd.DataFrame({'LANGUAGE_ID': id.index, 'LANGUAGE': languages})
     
     #find the maximum length of language
     lengths=df['Language'].str.len()
     maxlen=lengths.sort_values(ascending=False).iloc[0]
     print('Maximum length of title is ',maxlen)
-    
+
     #rename columns
     dfi.columns=['LANGUAGE_ID','LANGUAGE'] #use clip_id as genre_id here
     print(dfi)
