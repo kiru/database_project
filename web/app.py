0b993a13237d4c0c8c9f1fefa9f562f785cb6ed4
Hyunjii Cho <justcho5@gmail.com>
2018-05-17 18:28:33 +0200

    add checkboxes writer director producer

--- web/app.py
+++ web/app.py
@@ -46,6 +46,19 @@ def search_result(table):
         search_query(engine, input, names, 'select * from Clip where lower(clip_title) like :search', column_name,
                        "Clip")
 
+    elif table == 'Writer':
+        column_name = 'fullname'
+        search_query(engine, input, names, 'select * from writes, person where writes.person_id = person.person_id AND (lower(person.fullname) like :search)', column_name,
+                       "Writer")
+    elif table == 'Director':
+        column_name = 'fullname'
+        search_query(engine, input, names, 'select * from directs, person where directs.person_id = person.person_id AND (lower(person.fullname) like :search)', column_name,
+                       "Director")
+    elif table == 'Producer':
+        column_name = 'fullname'
+        search_query(engine, input, names, 'select * from produces, person where produces.person_id = person.person_id AND (lower(person.fullname) like :search)', column_name,
+                       "Producer")
+
     return render_template('search-result.html', tables=names, query=input, table_name = table)
 
 def search_query(engine, input, names, search, column, country="Country"):
@@ -58,24 +71,70 @@ def search_query(engine, input, names, search, column, country="Country"):
 @app.route('/search/')
 def search():
     input = request.args.get('query')
+    c = request.args.get('country')
+    l = request.args.get('language')
+    p = request.args.get('person')
+    clip = request.args.get('clips')
+    a = request.args.get('actor')
+    direct = request.args.get('director')
+    write = request.args.get('writer')
+    produce = request.args.get('producer')
+
     engine = createEngine()
 
     names = []
 
-    search_country(engine, input, names, 'select count(*) from Country where lower(COUNTRYNAME) like :search')
-    search_country(engine, input, names, 'select count(*) from Language where lower(language) like :search', "Language")
-    search_country(engine, input, names, 'select count(*) from Clip where lower(clip_title) like :search', "Clip")
-    search_country(engine, input, names, 'select count(*) from person where lower(fullname) like :search', "Person")
-    search_country(engine, input, names, 'select count(*) from acts, person where acts.person_id = person.person_id AND (lower(person.fullname) like :search)', "Actor")
+    if not (c or l or p or clip or a or direct or write or produce):
+        search_country(engine, input, names, 'select * from Country where lower(COUNTRYNAME) like :search limit 1')
+        search_country(engine, input, names, 'select * from Language where lower(language) like :search limit 1',
+                       "Language")
+        search_country(engine, input, names, 'select * from Clip where lower(clip_title) like :search limit 1', "Clip")
+        search_country(engine, input, names, 'select * from person where lower(fullname) like :search limit 1', "Person")
+        search_country(engine, input, names,
+                       'select * from acts, person where acts.person_id = person.person_id AND (lower(person.fullname) like :search) limit 1',
+                       "Actor")
 
+        search_country(engine, input, names,
+                       'select * from directs, person where directs.person_id = person.person_id AND (lower(person.fullname) like :search) limit 1',
+                       "Director")
+        search_country(engine, input, names,
+                       'select * from writes, person where writes.person_id = person.person_id AND (lower(person.fullname) like :search) limit 1',
+                       "Writer")
+        search_country(engine, input, names,
+                       'select * from produces, person where produces.person_id = person.person_id AND (lower(person.fullname) like :search) limit 1',
+                       "Producer")
+
+    else:
+        if c:
+            search_country(engine, input, names, 'select * from Country where lower(COUNTRYNAME) like :search limit 1')
+        if l:
+            search_country(engine, input, names, 'select * from Language where lower(language) like :search limit 1', "Language")
+        if clip:
+            search_country(engine, input, names, 'select * from Clip where lower(clip_title) like :search limit 1', "Clip")
+        if p:
+            search_country(engine, input, names, 'select * from person where lower(fullname) like :search limit 1', "Person")
+        if a:
+            search_country(engine, input, names, 'select * from acts, person where acts.person_id = person.person_id AND (lower(person.fullname) like :search) limit 1', "Actor")
+        if direct:
+            search_country(engine, input, names,
+                           'select * from directs, person where directs.person_id = person.person_id AND (lower(person.fullname) like :search) limit 1',
+                           "Director")
+        if write:
+            search_country(engine, input, names,
+                           'select * from writes, person where writes.person_id = person.person_id AND (lower(person.fullname) like :search) limit 1',
+                           "Writer")
+        if produce:
+            search_country(engine, input, names,
+                           'select * from produces, person where produces.person_id = person.person_id AND (lower(person.fullname) like :search) limit 1',
+                           "Producer")
     return render_template('search.html', tables=names, query=input)
 
 
 def search_country(engine, input, names, search, country="Country"):
     sql = text(search)
     result = engine.execute(sql, search="%{}%".format(input).lower())
-    row = result.fetchone();
-    if row[0] > 0:
+    row = result.first();
+    if row:
         names.append(country);
 
 
